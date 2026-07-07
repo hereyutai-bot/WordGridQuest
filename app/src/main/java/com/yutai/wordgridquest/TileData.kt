bf81b48ba6de10b8d1dfda5686cfc67407bdd8a7
Yutai <Hereyutai@gmail.com>
2026-07-07 14:43:40 +0800

    v1.4 move tile data to TileData

---
+++ app/src/main/java/com/yutai/wordgridquest/TileData.kt
@@ -0,0 +1,59 @@
+package com.yutai.wordgridquest
+
+val tileSlots = listOf(
+    TileSlot(1, row = 0, col = 0, layer = 0),
+    TileSlot(2, row = 0, col = 1, layer = 0),
+    TileSlot(3, row = 0, col = 2, layer = 0),
+    TileSlot(4, row = 0, col = 3, layer = 0),
+    TileSlot(5, row = 0, col = 4, layer = 0),
+    TileSlot(6, row = 0, col = 5, layer = 0),
+    TileSlot(7, row = 0, col = 6, layer = 0),
+
+    TileSlot(8, row = 1, col = 0, layer = 0),
+    TileSlot(9, row = 1, col = 1, layer = 0),
+    TileSlot(10, row = 1, col = 2, layer = 0),
+    TileSlot(11, row = 1, col = 3, layer = 0),
+    TileSlot(12, row = 1, col = 4, layer = 0),
+    TileSlot(13, row = 1, col = 5, layer = 0),
+    TileSlot(14, row = 1, col = 6, layer = 0),
+
+    TileSlot(15, row = 2, col = 0, layer = 0),
+    TileSlot(16, row = 2, col = 1, layer = 0),
+    TileSlot(17, row = 2, col = 2, layer = 0),
+    TileSlot(18, row = 2, col = 3, layer = 0),
+    TileSlot(19, row = 2, col = 4, layer = 0),
+    TileSlot(20, row = 2, col = 5, layer = 0),
+    TileSlot(21, row = 2, col = 6, layer = 0),
+
+    TileSlot(22, row = 3, col = 0, layer = 0),
+    TileSlot(23, row = 3, col = 1, layer = 0),
+    TileSlot(24, row = 3, col = 2, layer = 0),
+    TileSlot(25, row = 3, col = 3, layer = 0),
+    TileSlot(26, row = 3, col = 4, layer = 0),
+    TileSlot(27, row = 3, col = 5, layer = 0),
+    TileSlot(28, row = 3, col = 6, layer = 0),
+
+    TileSlot(29, row = 0, col = 1, layer = 1),
+    TileSlot(30, row = 0, col = 2, layer = 1),
+    TileSlot(31, row = 0, col = 3, layer = 1),
+    TileSlot(32, row = 0, col = 4, layer = 1),
+    TileSlot(33, row = 0, col = 5, layer = 1),
+
+    TileSlot(34, row = 1, col = 1, layer = 1),
+    TileSlot(35, row = 1, col = 2, layer = 1),
+    TileSlot(36, row = 1, col = 3, layer = 1),
+    TileSlot(37, row = 1, col = 4, layer = 1),
+    TileSlot(38, row = 1, col = 5, layer = 1),
+
+    TileSlot(39, row = 2, col = 2, layer = 1),
+    TileSlot(40, row = 2, col = 3, layer = 1),
+    TileSlot(41, row = 2, col = 4, layer = 1),
+
+    TileSlot(42, row = 0, col = 2, layer = 2),
+    TileSlot(43, row = 0, col = 3, layer = 2),
+    TileSlot(44, row = 0, col = 4, layer = 2),
+
+    TileSlot(45, row = 1, col = 2, layer = 2),
+    TileSlot(46, row = 1, col = 3, layer = 2),
+    TileSlot(47, row = 1, col = 4, layer = 2)
+)
