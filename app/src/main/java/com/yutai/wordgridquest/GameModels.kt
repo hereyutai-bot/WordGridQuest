0ba433118dadbe0c40fb298e9aad6580c4e147a4
Yutai <Hereyutai@gmail.com>
2026-07-07 14:46:32 +0800

    v1.5 move game models to GameModels

---
+++ app/src/main/java/com/yutai/wordgridquest/GameModels.kt
@@ -0,0 +1,55 @@
+package com.yutai.wordgridquest
+
+enum class Screen {
+    HOME,
+    MODE_SELECT,
+    LETTER_TILE,
+    HINT_WORD,
+    RANKING,
+    STUDY_RECORD
+}
+
+data class HintWordQuestion(
+    val word: String,
+    val meaning: String,
+    val clue: String,
+    val missingAnswer: String
+)
+
+data class GameResult(
+    val score: Int,
+    val correctCount: Int,
+    val wrongCount: Int,
+    val totalQuestions: Int
+)
+
+data class LetterTile(
+    val id: Int,
+    val letter: String,
+    val row: Int,
+    val col: Int,
+    val layer: Int,
+    val isSelected: Boolean = false,
+    val isRemoved: Boolean = false
+)
+
+data class TileSlot(
+    val id: Int,
+    val row: Int,
+    val col: Int,
+    val layer: Int
+)
+
+data class LetterTileGameResult(
+    val score: Int,
+    val successWordCount: Int,
+    val removedTileCount: Int,
+    val remainingTileCount: Int,
+    val hintUsedCount: Int
+)
+
+data class HintResult(
+    val word: String,
+    val meaning: String,
+    val tileIds: List<Int>
+)
